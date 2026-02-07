6b72d1bcef8eb1b52795b19440e356eba6b4d077
edw_flores <hpg9sppwrj@privaterelay.appleid.com>
2026-02-06 22:50:02 -0600

    first commit

--- rocket.py
+++ rocket.py
@@ -1,21 +1,48 @@
 import math
 
-#Rocket Parameters
 gravity = 9.81 #m/s^2
-initial_mass = int(input("Initial Mass (kg): "))
-final_mass = int(input("Final Mass (kg): "))
-specific_impulse = int(input("Specific Impulse (s): "))
 
-#Exhaust Velocity
-exhaust_velocity = specific_impulse * gravity
-print(f"Exhaust velocity is {exhaust_velocity} m/s")
+def get_rocket_parameters():
+    initial_mass = float(input("Initial Mass (kg): "))
+    final_mass = float(input("Final Mass (kg): "))
 
-#Mass Ratio
-mass_ratio = initial_mass / final_mass
-ln_mass_ratio = math.log(mass_ratio)
+    return initial_mass, final_mass
 
-print('')
+def get_simulation_parameters(gravity):
+    specific_impulse = float(input("Specific Impulse (s): "))
+    exhaust_velocity = specific_impulse * gravity
+    print(f"Exhaust velocity is {exhaust_velocity} m/s")
 
-#Change in Velocity
-delta_velocity = exhaust_velocity * ln_mass_ratio
-print(f"Change in velocity is {delta_velocity} m/s")
+    return exhaust_velocity
+
+def calculate_delta_velocity(initial_mass, final_mass, exhaust_velocity):
+    mass_ratio = initial_mass / final_mass
+    ln_mass_ratio = math.log(mass_ratio)
+
+    print('')
+
+    delta_velocity = exhaust_velocity * ln_mass_ratio
+    print(f"Maximum theoretical speed is {delta_velocity} m/s")
+
+    return delta_velocity
+
+def calculate_mass_ratio(exhaust_velocity):
+    delta_velocity = float(input("Maximum Theoretical Speed (m/s): "))
+
+    print('')
+
+    mass_ratio = math.exp(delta_velocity / exhaust_velocity)
+    print(f"Fuel fraction is {mass_ratio}%")
+
+    return mass_ratio
+
+response = input("Rocket Parameter or Change in Velocity [X/Y]: ")
+
+if response == "X":
+    exhaust_velocity = get_simulation_parameters(gravity)
+    calculate_mass_ratio(exhaust_velocity)
+
+elif response == "Y":
+    initial_mass, final_mass = get_rocket_parameters()
+    exhaust_velocity = get_simulation_parameters(gravity)
+    calculate_delta_velocity(initial_mass, final_mass, exhaust_velocity)
